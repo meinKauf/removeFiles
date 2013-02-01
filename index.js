b101e9bb8237c6447c38b2e18c998b63384294fb
Thomas Fritz <fritztho@gmail.com>
2013-02-01 11:50:46 +0100

    Refactored executable

--- index.js
+++ index.js
@@ -1,21 +1,5 @@
 var deleteInDirectory = require('./lib/deleteInDirectory');
 var comparators = require('./lib/comparators');
 
-module.exports = function (startPath, options) {
-	var options = options || {};
-	deleteInDirectory(startPath, comparators.ctime, options, function (error, meta) {
-		var size = meta.size;
-		var files = meta.files;
-		var unit = "byte";
-		if (size > 1048576) {
-			size = size / 1048576;
-			unit = "mb";
-		} else if (size > 1024) {
-			size = size / 1024;
-			unit = "kb";
-		}
-		size = Math.round(size * 100) / 100;
-		var dryRunString = options.dryRun ? "(dryrun) " : "";
-		console.log(dryRunString + "Deleted " + files + " files with a total size of " + size + unit + ".");
-	});
-}
+module.exports.removeFiles = deleteInDirectory;
+module.exports.comparators = comparators;
